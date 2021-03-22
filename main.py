5feb85e57630bf3459c017b8289f6756e99a7c95
EthanSargent23 <817233@mystma.org>
2021-03-22 15:25:35 +0000

    Mad Libs

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-nOun1 = " space"
-nOun2 = "europe"
-nOun3 = "pigs"
-aDj1 = "terrible"
-aDj2 = "weird"
-aDj3 = "ferious"
-aDj4 = "slippery"
-aDj5 = "jittery"
-aDj6 = "glorious"
-aDj7 = "ambitious" 
-vEh = " lamborgini"
-print("Star Wars is a " + aDj1 + nOun1 + " of " + aDj2 + " Versus evil in a " + nOun2 + " far far away. There are " + aDj3 + " battle between " + aDj4 + vEh + " in " + aDj5 + " space and " + aDj6 + " duels with " + nOun3 + " called " + aDj7 + " sabers.")
+nOun1 = input("enter a noun\n")
+nOun2 = input("enter a noun\n")
+nOun3 = input("enter a noun\n")
+aDj1 = input("enter a adjective\n")
+aDj2 = input("enter a adjective\n")
+aDj3 = input("enter a adjective\n")
+aDj4 = input("enter a adjective\n")
+aDj5 = input("enter a adjective\n")
+aDj6 = input("enter a adjective\n")
+aDj7 = input("enter a adjective\n") 
+vEh = input("enter a Vehicle\n")
+print("Star Wars is a " + aDj1 + " " + nOun1 + " of " + aDj2 + " Versus evil in a " + nOun2 + " far far away. There are " + aDj3 + " battle between " + aDj4 + " " + vEh + " in " + aDj5 + " space and " + aDj6 + " duels with " + nOun3 + " called " + aDj7 + " sabers.")
