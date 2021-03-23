7071f33315431fe31ff3acd1b5244dba8851c0ca
EthanSargent23 <817233@mystma.org>
2021-03-23 15:18:53 +0000

    Math

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-nOun1 = input("enter a noun\n")
-nOun2 = input("enter a noun\n")
-nOun3 = input("enter a noun\n")
-aDj1 = input("enter a adjective\n")
-aDj2 = input("enter a adjective\n")
-aDj3 = input("enter a adjective\n")
-aDj4 = input("enter a adjective\n")
-aDj5 = input("enter a adjective\n")
-aDj6 = input("enter a adjective\n")
-aDj7 = input("enter a adjective\n") 
-vEh = input("enter a Vehicle\n")
+def addTwoNumbers(x, y):
+    return x + y
 
-print("Star Wars is a " + aDj1 + " " + nOun1 + " of " + aDj2 + " Versus evil in a " + nOun2 + " far far away. There are " + aDj3 + " battle between " + aDj4 + " " + vEh + " in " + aDj5 + " space and " + aDj6 + " duels with " + nOun3 + " called " + aDj7 + " sabers.")
+print(addTwoNumbers(5, 7))
